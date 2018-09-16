b4867e7a73a5865ad84edb6d94910faf042a99ad
Abigail Lu <abigaillu@macbook-pro-2.home>
2018-09-16 14:36:47 -0700

    Added List Interface

---
+++ src/IDataStructures/List.java
@@ -0,0 +1,11 @@
+package IDataStructures;
+
+public interface List {
+
+    public Object add(Object o);
+    public Object remove(Object o);
+    public void checkSize(Object[] aList);
+    public void contains(Object o);
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,19 @@
+/**
+ * Developer: Abigail Lu
+ * Date: 9/16/2018
+ * Description: Implementing common data structures:
+ * Lists:
+ *  Linked List
+ *  ,Queue
+ *  ,Stack
+ *  ,Deque
+ *  ,Set
+ *
+ * Trees:
+ *  Binary Tree
+ *  ,N-ary Tree
+ */
+
 public class Main {
 
     public static void main(String[] args) {
