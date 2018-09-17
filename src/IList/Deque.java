6f2477d64f7726abfd8b779a06754f634fb5e332
Abigail Lu <abigaillu@macbook-pro-2.attlocal.net>
2018-09-16 23:43:40 -0700

    Setting up interfaces and Lists.

---
+++ src/IList/Deque.java
@@ -0,0 +1,24 @@
+package IList;
+
+public class Deque implements List {
+
+    @Override
+    public boolean add(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public int checkSize() {
+        return 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+}
---
+++ src/IList/List.java
@@ -0,0 +1,13 @@
+package IList;
+
+public interface List {
+
+    boolean add(Object o);
+
+    boolean remove(Object o);
+
+    int checkSize();
+
+    boolean contains(Object o);
+
+}
